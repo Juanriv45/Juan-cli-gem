eefd24930d88c937697960c3e7406e6ad44861ba
Juan Rivera <juanriv45@gmail.com>
2016-09-08 01:21:22 +0000

    added steps to enable nokogiri

--- lib/JR_Cli_Gem.rb
+++ lib/JR_Cli_Gem.rb
@@ -1,3 +1,6 @@
 require_relative "./JR_Cli_Gem/version"
 require_relative "./JR_Cli_Gem/cli"
 require_relative "./JR_Cli_Gem/movie"
+
+require 'open-uri'
+require 'nokogiri'
--- lib/JR_Cli_Gem/movie.rb
+++ lib/JR_Cli_Gem/movie.rb
@@ -1,13 +1,12 @@
 class AlamoTimes::Movie
   attr_accessor :name, :times
   def self.today
-    puts "Joe Dirt 3:00 PM, 4:00 PM, 5:00 PM"
-
     movie_1 = self.new
     movie_1.name = "Joe Dirt"
     movie_1.times = ["3:00 PM","4:00 PM", "5:00 PM"]
 
-    movie_1
+    puts "#{movie_1.name}"
+
   end
 
 
