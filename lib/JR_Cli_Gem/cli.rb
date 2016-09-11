a62e06df5b44882d5cc5a1f8aaedc11a268c3ccd
Juan Rivera <juanriv45@gmail.com>
2016-09-11 00:51:54 +0000

    Moved list of movies to a new class array

--- lib/JR_Cli_Gem/cli.rb
+++ lib/JR_Cli_Gem/cli.rb
@@ -2,14 +2,17 @@ class MoviesComingSoon::CLI
 
   def call
     puts "This is a list of the movies that are coming soon (IMBD Site)"
+    retrieve_movies
     list_movies
     menu
   end
 
   def list_movies
-    puts MoviesComingSoon::Movies.today
+    puts MoviesComingSoon::Movie.movie_list
   end
-
+  def retrieve_movies
+    MoviesComingSoon::Movie.today
+  end
   def menu
     puts "Enter the name of the movie of which you would like to see a description for or enter list to see the list again or enter exit:"
     input = nil
--- lib/JR_Cli_Gem/movie.rb
+++ lib/JR_Cli_Gem/movie.rb
@@ -1,6 +1,6 @@
 class MoviesComingSoon::Movie
   attr_accessor :name, :description, :new
-
+  @@movie_list = []
 
   def self.today
     self.scrape_IMBD
@@ -8,12 +8,20 @@ class MoviesComingSoon::Movie
   end
 
   def self.scrape_IMBD
-    movies = []
+    count = 0
+
     doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/"))
-    movies = doc.css(".overview-top h4 a").collect do |movie|
-      movie.text
+
+
+    movies = doc.css(".overview-top h4 a").each do |movie|
+      @@movie_list[count] = movie.text
+      count+= 1
     end
 
+  end
+
+  def self.movie_list
+    @@movie_list
   end
 
 end
