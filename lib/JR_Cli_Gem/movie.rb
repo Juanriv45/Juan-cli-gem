a2e905c3ebf163309e6a9998ca36f2fcae3082d8
Juan Rivera <juanriv45@gmail.com>
2016-09-12 02:04:52 +0000

    scraped movie descriptions

--- lib/JR_Cli_Gem/movie.rb
+++ lib/JR_Cli_Gem/movie.rb
@@ -1,27 +1,36 @@
 class MoviesComingSoon::Movie
-  attr_accessor :name, :description, :new
+
   @@movie_list = []
+  @@description_list = []
 
   def self.today
     self.scrape_IMBD
-
   end
 
   def self.scrape_IMBD
-    count = 0
 
     doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/"))
 
-
+    movie_count = 0
     movies = doc.css(".overview-top h4 a").each do |movie|
-      @@movie_list[count] = movie.text
-      count+= 1
+      @@movie_list[movie_count] = movie.content.gsub(/[(]\w+[)]/, "")
+      movie_count+= 1
+    end
+
+    description_count = 0
+    descriptions = doc.search(".overview-top .outline").each do |descriptions|
+      @@description_list[description_count] = descriptions.content.strip
+      description_count+= 1
     end
 
   end
 
   def self.movie_list
     @@movie_list
+  end
+
+  def self.description_list
+    @@description_list
   end
 
 end
