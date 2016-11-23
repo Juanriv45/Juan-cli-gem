fd88515a67bae8bec21c439dddc70a114b0c11b2
Juan Rivera <juanriv45@gmail.com>
2016-11-23 05:31:13 +0000

    Converted the movie hashes into objects

--- lib/movies_coming_soon/movie.rb
+++ lib/movies_coming_soon/movie.rb
@@ -1,37 +1,41 @@
 class MoviesComingSoon::Movie
-
+  attr_reader :title, :description
   @@movie_list = []
+
+  def initialize(title, description)
+    @title = title
+    @description = description
+  end
 
   def self.scrape_IMDB
     doc = Nokogiri::HTML(open("http://www.imdb.com/movies-coming-soon/"))
     doc.css(".overview-top").collect do |movie|
-      @@movie_list << {
-        :title => movie.css('h4 a').text.gsub(/[(]\w+[)]/, "").strip,
-        :description => movie.css('div.outline').text.strip
-      }
-    end
 
-  end
+      title = movie.css('h4 a').text.gsub(/[(]\w+[)]/, "").strip
+      description = movie.css('div.outline').text.strip
 
-  def self.movie_included?(input)
-    lower_case_list = []
-    @@movie_list.select do |movie|
-      lower_case_list << movie[:title].downcase
-    end
-    lower_case_list.include?(input)
-  end
+      @@movie_list << self.new(title, description)
 
-  def self.movie_description(input)
-    @@movie_list.select do |movie|
-      if movie[:title].downcase == input then
-        puts movie[:description]
-      end
     end
   end
 
   def self.list_of_movies
     @@movie_list.select do |movie|
-        puts movie[:title]
+        puts movie.title
+    end
+  end
+
+  def self.movie_included?(input)
+    @@movie_list.any? do |movie|
+      movie.title.downcase == input
+    end
+  end
+
+  def self.movie_description(input)
+    @@movie_list.select do |movie|
+      if movie.title.downcase == input then
+        puts movie.description
+      end
     end
   end
 
